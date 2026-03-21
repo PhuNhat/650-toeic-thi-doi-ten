d18fac35369f3d50c9b40563fd2705f8a50eb1fb
kkitb004-afk <kkitb004@gmail.com>
2026-03-21 22:30:31 +0700

    fix database

--- src/server/Database.java
+++ src/server/Database.java
@@ -101,35 +101,3 @@ public class Database {
         return result;
     }
 }
-
-    public void insertData(String id, String content, String time, String status) {
-        String sSQL = "INSERT INTO " + tableName + " VALUES ('" + id + "','" + content + "','" + time + "','" + status + "')";
-        try {
-            stmt.executeUpdate(sSQL);
-        } catch (Exception e) {
-            System.out.println("SQLException: " + e.getMessage());
-        }
-    }
-
-    public void delData(String id) {
-        try {
-            String sSQL = "DELETE FROM " + tableName + " WHERE id='" + id + "'";
-            stmt.executeUpdate(sSQL);
-        } catch (Exception e) {
-            System.out.println("SQLException: " + e.getMessage());
-        }
-    }
-
-    public String getAllData() {
-        String result = "";
-        try {
-            rs = stmt.executeQuery("SELECT * FROM " + tableName);
-            while (rs.next()) {
-                result += rs.getString("id") + " | " + rs.getString("content") + " | " + rs.getString("time") + " | " + rs.getString("status") + "\n";
-            }
-        } catch (Exception e) {
-            System.out.println("SQLException: " + e.getMessage());
-        }
-        return result;
-    }
-}
